e72461c7e07636e2364b1f1069a61c51de2f52e9
SavelevGeo <savelevgeo.post@gmail.com>
2026-09-16 22:18:59 +0300

    add work_types and works tables and views

--- ddl.sql
+++ ddl.sql
@@ -1,16 +1,31 @@
 .mode line
 
 DROP VIEW IF EXISTS authors_rus;
+DROP VIEW IF EXISTS works_rus;
+DROP VIEW IF EXISTS work_types_rus;
+
 DROP TABLE IF EXISTS author_names;
+DROP TABLE IF EXISTS work_names;
+DROP TABLE IF EXISTS work_type_names;
+
 DROP TABLE IF EXISTS languages;
+
 DROP TABLE IF EXISTS authors;
+
+DROP TABLE IF EXISTS work_types;
+DROP TABLE IF EXISTS works;
+
 DROP SEQUENCE IF EXISTS author_id;
+DROP SEQUENCE IF EXISTS work_type_id;
+DROP SEQUENCE IF EXISTS work_id;
+
 
 CREATE TABLE languages AS
 FROM read_json('languages.json');
 
 ALTER TABLE languages
 ADD PRIMARY KEY (iso_639_2);
+
 
 CREATE SEQUENCE author_id;
 
@@ -38,11 +53,55 @@ LEFT JOIN author_names AN_rus ON (
     AND AN_rus.iso_639_2 = 'rus'
 );
 
+
+CREATE SEQUENCE work_type_id;
+
+CREATE TABLE work_types (
+    id INT PRIMARY KEY
+        DEFAULT nextval('work_type_id')
+);
+
+CREATE TABLE work_type_names (
+    work_type INT REFERENCES work_types(id),
+    iso_639_2 VARCHAR
+        REFERENCES languages(iso_639_2),
+    names VARCHAR[],
+    PRIMARY KEY (work_type, iso_639_2)
+);
+
+CREATE VIEW work_types_rus AS
+SELECT array_to_string(WT_rus.names, ' ') AS name
+FROM work_types WT
+LEFT JOIN work_type_names WT_rus ON (
+    WT.id = WT_rus.work_type
+    AND WT_rus.iso_639_2 = 'rus'
+);
+
+
 BEGIN TRANSACTION;
     INSERT INTO authors (is_dead) VALUES (false);
     INSERT INTO author_names VALUES (
-        currval('author_id'), 'rus', ['Улицкая']
+        currval('author_id'),
+        'rus',
+        ['Людмила', 'Улицкая']
     );
 COMMIT;
 SELECT * FROM authors_rus;
 
+BEGIN TRANSACTION;
+    INSERT INTO work_types DEFAULT VALUES;
+    INSERT INTO work_type_names VALUES (
+        currval('work_type_id'),
+        'rus',
+        ['Сборник', 'рассказов']
+    );
+COMMIT;
+BEGIN TRANSACTION;
+    INSERT INTO work_types DEFAULT VALUES;
+    INSERT INTO work_type_names VALUES (
+        currval('work_type_id'),
+        'rus',
+        ['Рассказ']
+    );
+COMMIT;
+SELECT * FROM work_types_rus;
